4f312a26206231673e7be592ef2994e33f4cef71
alivasquez16 <aylingonzalezvasquez16@gmail.com>
2025-07-29 15:29:50 -0500

    Aplicación foto - Parte 1

---
+++ 1_repaso_conceptos/java/interaccion.js
@@ -0,0 +1,9 @@
+let imagen = document.querySelector(".imagen-principal")
+let boton1 = document.querySelector(".boton-1")
+let boton2 = document.querySelector(".boton-2")
+let boton3 = document.querySelector(".boton-3")
+
+boton1.addEventListener('click', boton1Funcion)
+function FlorFuncion(){
+   
+}
