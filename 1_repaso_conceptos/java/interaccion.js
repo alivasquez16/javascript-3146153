e741a861f5ce2d7bd2a239e689fc954525c2449a
alivasquez16 <aylingonzalezvasquez16@gmail.com>
2025-07-30 15:27:43 -0500

    Aplicació Terminada

--- 1_repaso_conceptos/java/interaccion.js
+++ 1_repaso_conceptos/java/interaccion.js
@@ -1,10 +1,10 @@
 let imagen = document.querySelector(".imagen");
-let botones = document.querySelectorAll(".botones button");
+/* let botones = document.querySelectorAll(".botones button");
 let efectoActivo = null;
 
 botones.forEach(boton => {
   boton.addEventListener("click", () => {
-    const efecto = boton.classList[0]; 
+    let efecto = boton.classList[0]; 
 
     imagen.classList.remove("blanco-y-negro", "desenfocar", "zoom-rotar");
 
@@ -32,4 +32,36 @@ function quitarEstiloActivo() {
 function marcarBotonActivo(botonActivo) {
   quitarEstiloActivo();
   botonActivo.classList.add("activo");
+} */
+
+
+
+
+
+/* 1. capturar o identificar variables */
+const pantalla = document.querySelector ("#imagen") /* para capturar o llamar la imagen y '# o .' si es id o class */
+const boton1 = document.querySelector ("#boton1") 
+const boton2 = document.querySelector ("#boton2") 
+const boton3 = document.querySelector ("#boton3") 
+
+/* 2. funciones  */
+function blancoynegro (){
+  console.log ("Imagen modificada")
+  pantalla.style.filter = "grayscale(100%)"
+/*   boton1.style.background = "blueviolet"
+  boton1.style.color = "black"
+  boton1.style.height = "200px"
+  boton1.style.borderRadius = "500px" */
 }
+
+function desenfocar (){
+  pantalla.style.filter = "blur(5px)"
+}
+
+function zoomyrotar (){
+  pantalla.style.transform = "scale(1.1) rotate(4deg)"
+}
+/* 3. eventos */
+boton1.addEventListener("click",blancoynegro)
+boton2.addEventListener("click",desenfocar)
+boton3.addEventListener("click",zoomyrotar)
