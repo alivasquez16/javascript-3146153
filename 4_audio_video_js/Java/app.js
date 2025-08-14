55d729b499349cab0a80e465ef4a1af929682d82
alivasquez16 <aylingonzalezvasquez16@gmail.com>
2025-08-14 15:32:26 -0500

    Audio y video - Parte 1

---
+++ 4_audio_video_js/Java/app.js
@@ -0,0 +1,11 @@
+const video = document.querySelector ('#video')
+const playVideo = document.querySelector ('#play-video')
+const stopVideo = document.querySelector ('#stop-video')
+const img1 = document.querySelector ('#play-stop-img1')
+const img2 = document.querySelector ('#play-stop-img2')
+
+console.log(video)
+console.log(playVideo)
+console.log(stopVideo)
+console.log(img1)
+console.log(img2)
