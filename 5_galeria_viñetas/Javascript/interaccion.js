bf85f7c437f945d10bed7a8216bc95e5479a0562
alivasquez16 <aylingonzalezvasquez16@gmail.com>
2025-09-01 17:13:25 -0500

    Finalizado

--- 5_galeria_viñetas/Javascript/interaccion.js
+++ 5_galeria_viñetas/Javascript/interaccion.js
@@ -56,12 +56,12 @@ const escenas = document.querySelectorAll('.escena')
 const btnAnterior = document.querySelector('.anterior')
 const btnSiguiente = document.querySelector('.siguiente')
 const miniaturas = document.querySelectorAll('.miniaturas img')
-let indice = 0 
+let indice = 0
 
 //console.log(escenas)
 //console.log(escenas[1])
 
-function mostrarEscena(i){
+function mostrarEscena(i) {
   //console.log(escenas[i])
 
   for (let j = 0; j < escenas.length; j++) {
@@ -74,26 +74,96 @@ function mostrarEscena(i){
 
 //Botón siguiente
 
-btnSiguiente.addEventListener("click", function(){
+btnSiguiente.addEventListener("click", function () {
   indice = indice + 1
-  if(indice >= escenas.length){
+  if (indice >= escenas.length) {
     indice = 0  //Para que vuelva a la primera escena
   }
   mostrarEscena(indice)
 })
 
 //Botón Anterior
-btnAnterior.addEventListener("click", function(){
+btnAnterior.addEventListener("click", function () {
   indice = indice - 1
-  if(indice < 0){
+  if (indice < 0) {
     indice = escenas.length - 1 //Para que vaya a la última escena
   }
   mostrarEscena(indice)
-}) 
+})
 
 //Miniaturas 
 miniaturas.forEach((miniatura, i) => {
-  miniatura.addEventListener("click", function(){
+  miniatura.addEventListener("click", function () {
     mostrarEscena(i)
   })
 });
+
+//Botones - Audios 
+const audio1 = document.querySelector(".audio1");
+const audiodelaudio = document.querySelector(".audio1 audio");
+const parrafo = document.querySelector(".audio1 p");
+
+audio1.addEventListener("click", activar)
+
+function activar() {
+  console.log("hola")
+  parrafo.textContent = "⏸"
+  audiodelaudio.play();
+  audio1.removeEventListener("click", activar)
+  audio1.addEventListener("click", desactivar);
+
+}
+
+function desactivar() {
+  audiodelaudio.pause();
+  parrafo.textContent = "▶";
+  audio1.removeEventListener("click", desactivar);
+  audio1.addEventListener("click", activar);
+}
+
+
+
+
+
+const btnAudio2 = document.querySelector(".audio2");
+const audio2 = document.querySelector(".audio2 audio");
+const parrafo2 = document.querySelector(".audio2 p");
+
+
+btnAudio2.addEventListener("click", activa2);
+
+function activa2() {
+  audio2.play();
+  parrafo2.textContent = "⏸";
+  btnAudio2.removeEventListener("click", activa2);
+  btnAudio2.addEventListener("click", desactiva2);
+}
+
+function desactiva2() {
+  audio2.pause();
+  parrafo2.textContent = "▶";
+  btnAudio2.removeEventListener("click", desactiva2);
+  btnAudio2.addEventListener("click", activa2);
+}
+
+
+const btnAudio3 = document.querySelector(".audio3");
+const audio3 = document.querySelector(".audio3 audio");
+const parrafo3 = document.querySelector(".audio3 p");
+
+btnAudio3.addEventListener("click", activa3);
+
+function activa3() {
+  audio3.play();
+  parrafo3.textContent = "⏸";
+  btnAudio3.removeEventListener("click", activa3);
+  btnAudio3.addEventListener("click", desactiva3);
+}
+
+function desactiva3() {
+  audio3.pause();
+  parrafo3.textContent = "▶";
+  btnAudio3.removeEventListener("click", desactiva3);
+  btnAudio3.addEventListener("click", activa3);
+}
+
