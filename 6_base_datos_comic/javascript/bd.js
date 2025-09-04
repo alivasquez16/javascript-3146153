e957c1b827828e8aefa5ec01bfbaf1d3e0975695
alivasquez16 <aylingonzalezvasquez16@gmail.com>
2025-09-04 15:57:08 -0500

    Base de Datos - Js

---
+++ 6_base_datos_comic/javascript/bd.js
@@ -0,0 +1,44 @@
+const comic = {
+    "nombreComic": "Saldo Negativo",
+    "categoria": "Narrativas Distopica",
+    "Descripcion": "“Saldo Negativo” es un cómic distópico que retrata una sociedad donde el valor humano se mide como un saldo. A través de Zaya y otros marginados, la historia muestra las grietas de un sistema corrupto y la lucha por recuperar memoria, voz y resistencia en medio del olvido.",
+    "Personajes": ["Zaya", "Inara", "Mylo"],
+    "cantidadCap": 5,
+    "Capitulos": [
+        {
+           " nombreCap": "Saldo Actualizo",
+           " miniatura": "./assets/capitulo1.jpeg",
+           " video": "video/capitulo1.mp4",
+           " descripcion": "Zaya descubre que su saldo ha sido actualizado a NEGATIVO, quedando fuera del sistema. Pierde el acceso a comida, estudios y refugio, enfrentando por primera vez el vacío y la invisibilidad de ser una “olvidada” en Cárdena.",
+           " personajes": "Zaya (Protagonista)"
+        },
+        {
+           " nombreCap": "La Grieta",
+           " miniatura": "./assets/capitulo2.jpeg",
+           " video": "video/capitulo1.mp4",
+           " descripcion": "Mientras busca sobrevivir, Zaya encuentra símbolos y mensajes ocultos que revelan que no está sola: existen grietas en el control del sistema.",
+           " personajes": "Zaya (Protagonista)"
+        },
+        {
+           " nombreCap": "Hexadecimal",
+           " miniatura": "./assets/capitulo3.jpeg",
+           " video": "video/capitulo1.mp4",
+           " descripcion": "Zaya se cruza con los rastros de Inara y Mylo, quienes desde las sombras filtran información y dejan pistas que cuestionan la “Reforma del Valor”.",
+           " personajes": "Zaya (Protagonista), Inara (Secundario), Mylo (Secundario)"
+        },
+        {
+           " nombreCap": "Ruido",
+           " miniatura": "./assets/capitulo4.jpeg",
+           " video": "video/capitulo1.mp4",
+           " descripcion": "Los protagonistas logran unir esfuerzos para transmitir pruebas de las exclusiones, rompiendo el silencio con un mensaje que sacude a la ciudad.",
+           " personajes":  "Zaya (Protagonista), Inara (Secundario), Mylo (Secundario)"
+        },
+        {
+           " nombreCap": "EXISTIR",
+           " miniatura": "./assets/capitulo5.jpeg",
+           " video": "video/capitulo1.mp4",
+           " descripcion": "El sistema tambalea ante la verdad expuesta. Aunque no derriban el régimen, Zaya, Inara y Mylo siembran memoria y conciencia en quienes habían olvidado.",
+           " personajes":  "Zaya (Protagonista), Inara (Secundario), Mylo (Secundario)"
+        }
+    ]
+}
